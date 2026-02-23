8455227df3af2716c42bb00edfdf76b14f6f809e
Franco Barberis <francobarberissic3@gmail.com>
2026-02-23 11:12:14 -0300

    Commit inicial con boilerplate de SpringBoot

---
+++ src/main/java/com/franco/gymplanner/GymplannerApiApplication.java
@@ -0,0 +1,13 @@
+package com.franco.gymplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymplannerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymplannerApiApplication.class, args);
+	}
+
+}
